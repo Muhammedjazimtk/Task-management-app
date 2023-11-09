63d53add141450e8cf4d6958a2221b21f1ad87a7
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2023-11-09 10:44:27 +0530

    last

--- task_front/src/App.jsx
+++ task_front/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="flex justify-center items-center font-bold text-[#38bdf8] text-2xl bg-[#1e293b] w-full h-[100px]">
         {day}
       </div>
-      <div>kunna</div>
+      <div>k</div>
       <div>khshashbshkbx</div>
       <div>poda </div>
     </div>
