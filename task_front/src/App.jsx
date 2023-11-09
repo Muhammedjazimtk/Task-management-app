bded987f3edd239921395f846304e49f54afa162
Sreehari <sreeharimurali8@gmail.com>
2023-11-09 10:32:25 +0530

    sreehari first commit

--- task_front/src/App.jsx
+++ task_front/src/App.jsx
@@ -14,6 +14,9 @@ function App() {
       <div className="flex justify-center items-center font-bold text-[#38bdf8] text-2xl bg-[#1e293b] w-full h-[100px]">
         {day}
       </div>
+      <div>
+        test
+      </div>
     </div>
   );
 }
