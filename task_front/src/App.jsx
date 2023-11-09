3cbb57376281ecd55606fc22011f78e9e2d57b7f
Sreehari <sreeharimurali8@gmail.com>
2023-11-09 11:21:35 +0530

    updated header

--- task_front/src/App.jsx
+++ task_front/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
 function App() {
   let date = new Date();
@@ -11,8 +12,10 @@ function App() {
 
   return (
     <div className="flex flex-col items-center gap-4 h-screen w-full bg-[#0f172a]">
-      <div className="flex justify-center items-center font-bold text-[#38bdf8] text-2xl bg-[#1e293b] w-full h-[100px]">
-        {day}
+      <div className="flex justify items-center font-bold text-[#38bdf8] text-2xl bg-[#1e293b] w-full h-[100px] pl-4 pr-4">
+        <p className="w-1/3 text-xl"> Tast Manager App</p>
+        <p className="w-1/3 text-center">{day}</p>
+        <div className="w-1/3 text-end"><AccountCircleRoundedIcon fontSize="large"/></div>
       </div>
       <div>khshashbshkbx</div>
       <div>poda </div>
