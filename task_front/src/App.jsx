5660cb346e6dd93ed7c0f88ad54ad6938f587f99
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2023-11-09 09:32:13 +0530

    initial commit

--- task_front/src/App.jsx
+++ task_front/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="h-screen w-full bg-black text-white">init</div>;
+  return <div className="h-screen w-full bg-[#0f172a] text-white">init</div>;
 }
 
 export default App;
