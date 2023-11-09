bd28b184427dce4ccb9e77c26213dddcec8d6464
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2023-11-09 10:34:07 +0530

    jazim test 2

--- task_front/src/App.jsx
+++ task_front/src/App.jsx
@@ -14,9 +14,8 @@ function App() {
       <div className="flex justify-center items-center font-bold text-[#38bdf8] text-2xl bg-[#1e293b] w-full h-[100px]">
         {day}
       </div>
-      <div>
-        test
-      </div>
+      <div>test</div>
+      <div>khshashbshkbx</div>
     </div>
   );
 }
