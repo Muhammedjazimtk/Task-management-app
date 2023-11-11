bcfc25a70a2c3c8e8d258df19dae1240f08097d9
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2023-11-11 17:27:00 +0530

    backend dependencies installed

--- back/app.js
+++ back/app.js
@@ -1,9 +1,23 @@
-var express = require("express");
+const express = require("express");
+const mongoose = require("mogoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.get("/signUP", (req, res) => {
+  res.send("heo");
+});
+
+app.post("/signUP", (req, res) => {
+  res.send("hellooo");
 });
 
 app.listen(3000, (req, res) => {
